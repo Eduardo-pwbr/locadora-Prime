afe06183f76e8c03aa4c04ac75a52cb298f07e3d
Eduardo-pwbr <eduardo.f.o.rodrigues@hotmail.com>
2026-01-06 03:51:18 -0300

    first commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -12,7 +12,7 @@ export default function Home() {
     async function loadFilmes() {
       const response = await api.get('movie/now_playing', {
         params: {
-          api_key: 'b0a0c8bfa3379431090ea52c3bb2110e', 
+          api_key: process.env.REACT_APP_API_KEY, 
           language: 'pt-BR',
           page: 1,
         }
